12bd927d4f0fddb367f4b32d7fef87e0dc01cbfc
Sheldon <lhanco@gmail.com>
2024-03-11 23:27:04 +0700

    Day 22: Render the app titles in two columns.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
+import pandas
 
-st.set_page_config(layout="wide")
+st.set_page_config()
 
 col1, col2 = st.columns(2)
 
@@ -8,8 +9,42 @@ with col1:
     st.image("images/photo.png")
 
 with col2:
-    st.title("Ardit Sulce")
+    st.title("Ly Han Co")
     content = """
-    I am Ly Han Co. I have worked 5 years as Software Engineer at DI Central Corporation and 3 years as System Analyst at Bayer Vietnam. So I have experienced in using some software such as ERP (SAP), SQL Server, C#, .Net, CRM. Besides, I have experienced in develop applications and tools for different departments included accountant, human resources, financial and production and also help them to increase overall efficiency by reducing the time and capital invested in ERP systems.
+    Hi, I am Ly Han Co. I have worked 5 years as Software Engineer at DI Central and 3 years as System Analyst at Bayer Vietnam. My technical skills are SQL Server, C# .NET, CRM, Power Apps and Power BI in applications support for different Accounting, Human resources, Financial and Production teams. This helped them to increase overall efficiency by reducing the time and capital invested in ERP systems.
     """
     st.info(content)
+
+content2 = """
+Below you can find some of the apps which I have built in Python. Feel free to contact me!
+"""
+st.write(content2)
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
